fcf5bfcbca67d3daed0abf0f735bfcfd4f9cc571
Stamesha Bello <stameshabello@gmail.com>
2022-09-03 20:40:55 -0500

    Made CarFactory methods static

--- car/car_factory.py
+++ car/car_factory.py
@@ -12,26 +12,31 @@ from rorschach import Rorschach
 from thovex import Thovex
 
 class CarFactory():
+    @staticmethod
     def create_calliope(self, current_date: date, last_service_date: date,
         current_mileage: int, last_service_mileage: int) -> Car:
         return Calliope(CapuletEngine(last_service_mileage, current_mileage),
             SpindlerBattery(last_service_date, current_date))
 
+    @staticmethod
     def create_glissade(self, current_date: date, last_service_date: date,
         current_mileage: int, last_service_mileage: int) -> Car:
         return Glissade(WilloughbyEngine(last_service_mileage, current_mileage),
             SpindlerBattery(last_service_date, current_date))
 
+    @staticmethod
     def create_palindrome(self, current_date: date, last_service_date: date,
         warning_light_on: bool) -> Car:
         return Palindrome(SternmanEngine(warning_light_on),
             SpindlerBattery(last_service_date, current_date))
 
+    @staticmethod
     def create_rorschach(self, current_date: date, last_service_date: date,
         current_mileage: int, last_service_mileage: int) -> Car:
         return Rorschach(WilloughbyEngine(last_service_mileage, current_mileage),
             NubbinBattery(last_service_date, current_date))
 
+    @staticmethod
     def create_thovex(self, current_date: date, last_service_date: date,
         current_mileage: int, last_service_mileage: int) -> Car:
         return Thovex(CapuletEngine(last_service_mileage, current_mileage),
